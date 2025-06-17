25d845e961ef9f6379781a82ed5af11033baf4f1
Roman Kravchenko <kravchenko-rn@ukr.net>
2025-06-17 18:41:46 +0300

    Added javafaker lib

--- src/main/java/com/avenga/onlinebookstoretest/api/service/BaseService.java
+++ src/main/java/com/avenga/onlinebookstoretest/api/service/BaseService.java
@@ -1,16 +1,16 @@
 package com.avenga.onlinebookstoretest.api.service;
 
 import com.avenga.onlinebookstoretest.config.TestContext;
+import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE;
 
 public abstract class BaseService {
-    protected final StackWalker stackWalker = StackWalker.getInstance(RETAIN_CLASS_REFERENCE);
-    protected TestContext testContext;
 
+    protected final StackWalker stackWalker = StackWalker.getInstance(RETAIN_CLASS_REFERENCE);
     @Autowired
-    public final void setTestContext(TestContext testContext) {
-        this.testContext = testContext;
-    }
+    protected TestContext testContext;
+    @Autowired
+    protected Faker faker;
 }
